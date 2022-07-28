9d72acca3008c382efd6927bfe5236a23817cbd4
denispo <denispolasek@centrum.cz>
2022-07-28 18:43:24 +0200

    wip

--- src/TigerApi/TigerGetAuthTokenRequest.php
+++ src/TigerApi/TigerGetAuthTokenRequest.php
@@ -3,19 +3,20 @@
 namespace TigerApi;
 
 use Nette\Http\IRequest;
+use TigerCore\Auth\BaseDecodedTokenData;
 use TigerCore\Auth\ICanGenerateAuthTokenForUser;
 use TigerCore\Auth\ICanDecodeRefreshToken;
 use TigerCore\Auth\ICurrentUser;
 use TigerCore\Exceptions\InvalidTokenException;
 use TigerCore\Payload\AuthTokenPayload;
 use TigerCore\Request\BaseRequest;
-use TigerCore\Request\IOnAddToPayload;
 use TigerCore\Request\RequestParam;
 use TigerCore\Response\BaseResponseException;
 use TigerCore\Response\ICanAddToPayload;
+use TigerCore\ValueObject\VO_BaseId;
 use TigerCore\ValueObject\VO_TokenPlainStr;
 
-abstract class TigerGetAuthTokenRequest extends BaseRequest implements IOnAddToPayload {
+abstract class TigerGetAuthTokenRequest extends BaseRequest {
 
   #[RequestParam('refreshtoken')]
   public VO_TokenPlainStr $refreshToken;
@@ -24,14 +25,18 @@ abstract class TigerGetAuthTokenRequest extends BaseRequest implements IOnAddToP
 
   private VO_TokenPlainStr|null $authToken = null;
 
-  public function __construct(
-    private ICanDecodeRefreshToken $refreshTokenDecoder,
-    private ICanGenerateAuthTokenForUser $authTokenGenerator) {
-  }
+  /**
+   * @param VO_TokenPlainStr $refreshToken
+   * @return BaseDecodedTokenData
+   * @throws  InvalidTokenException
+   */
+  abstract protected function onGetDecodedRefreshToken(VO_TokenPlainStr $refreshToken):BaseDecodedTokenData;
+
+  abstract protected function onGenerateNewAuthTokenForUser(VO_BaseId $userId):VO_TokenPlainStr;
 
   public function onMatch(ICurrentUser $currentUser, ICanAddToPayload $payload, IRequest $httpRequest): void {
     try {
-      $parsedRefreshToken = $this->refreshTokenDecoder->decodeRefreshToken($this->refreshToken);
+      $parsedRefreshToken = $this->onGetDecodedRefreshToken($this->refreshToken);
     } catch (InvalidTokenException $e) {
       throw new BaseResponseException($e->getMessage());
     }
@@ -40,7 +45,7 @@ abstract class TigerGetAuthTokenRequest extends BaseRequest implements IOnAddToP
       throw new BaseResponseException('invalid user id in token');
     }
 
-    $authToken = $this->authTokenGenerator->generateAuthToken($parsedRefreshToken->getUserId());
+    $authToken = $this->onGenerateNewAuthTokenForUser($parsedRefreshToken->getUserId());
 
     if (!$authToken->isEmpty()) {
       $payload->addToPayload(new AuthTokenPayload($this->authToken));
