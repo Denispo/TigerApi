006d3fea2b2292248d9e270d783ca0893868a0d2
denispo <denispolasek@centrum.cz>
2022-08-22 23:34:31 +0200

    wip

--- src/TigerApi/TigerRouter.php
+++ src/TigerApi/TigerRouter.php
@@ -3,17 +3,7 @@
 namespace TigerApi;
 
 use TigerCore\BaseRestRouter;
-use TigerCore\ICanAddRequest;
 
 abstract class TigerRouter extends BaseRestRouter {
-
-  protected abstract function onGetUserLoginRequest():TigerLoginUserRequest;
-  protected abstract function onGetAuthTokenRequest():TigerGenerateAuthTokenFromRefreshTokenRequest;
-
-  protected function onGetRoutes(ICanAddRequest $r) {
-    $r->addRequest('POST', $this->onGetUserLoginRequest());
-    $r->addRequest('POST', $this->onGetAuthTokenRequest());
-  }
-
 
 }
