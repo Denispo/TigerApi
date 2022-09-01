0efbaa00166f77da46b2d2b9dbbffe766006988b
denispo <denispolasek@centrum.cz>
2022-09-01 14:47:48 +0200

    wip logger

--- src/TigerApi/Logger/FileLineClass.php
+++ src/TigerApi/Logger/FileLineClass.php
@@ -4,25 +4,32 @@ namespace TigerApi\Logger;
 
 class FileLineClass implements IAmFileLineClass{
 
-  public function __construct() {
-    print_r(debug_backtrace(options: 2,limit: 2));
-    exit;
+  private int $line;
+  private string $file;
+  private string $class;
+  private string $methodOrFunction;
 
+  public function __construct() {
+    $data = debug_backtrace(options: 2,limit: 2);
+    $this->line = $data[0]['line'] ?? 0;
+    $this->file = $data[0]['file'] ?? '';
+    $this->class = $data[1]['class'] ?? '';
+    $this->methodOrFunction = $data[1]['function'] ?? '';
   }
 
   public function getLine(): int {
-    return '';
+    return $this->line;
   }
 
   public function getFile(): string {
-    return '';
+    return $this->file;
   }
 
   public function getClass(): string {
-    return '';
+    return $this->class;
   }
 
   public function getMethodOrFunction(): string {
-    return '';
+    return $this->methodOrFunction;
   }
 }
