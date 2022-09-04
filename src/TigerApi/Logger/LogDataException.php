3c83a744c783c6e84df6cf6390bca046106df9b7
denispo <denispolasek@centrum.cz>
2022-09-04 23:48:51 +0200

    wip Logger

--- src/TigerApi/Logger/LogDataException.php
+++ src/TigerApi/Logger/LogDataException.php
@@ -5,7 +5,7 @@ namespace TigerApi\Logger;
 class LogDataException extends BaseLogData {
 
   public function __construct(\Throwable $exception, array $customData = []) {
-    parent::__construct($exception->getMessage(),['CUSTOMDATA' => $customData,'EXCEPTION_TRACE' => $exception->getTrace()],$exception->getFile(),$exception->getLine());
+    parent::__construct($exception->getMessage(),['CUSTOMDATA' => $customData, 'EXCEPTION_TRACE' => $exception->getTrace()],$exception->getFile(),$exception->getLine());
 
   }
 
--- src/TigerApi/Logger/LogDataWarning.php
+++ src/TigerApi/Logger/LogDataWarning.php
@@ -4,11 +4,11 @@ namespace TigerApi\Logger;
 
 class LogDataWarning extends BaseLogData {
 
-  public function __construct(string $message, array $data, IAmFileLineClass|null $fileLineClass = null) {
+  public function __construct(string $message, array $customData = [], IAmFileLineClass|null $fileLineClass = null) {
     if ($fileLineClass) {
-      parent::__construct($message, $data, $fileLineClass->getFile(), $fileLineClass->getLine(), $fileLineClass->getClass(), $fileLineClass->getMethodOrFunction());
+      parent::__construct($message, $customData, $fileLineClass->getFile(), $fileLineClass->getLine(), $fileLineClass->getClass(), $fileLineClass->getMethodOrFunction());
     } else {
-      parent::__construct($message, $data);
+      parent::__construct($message, $customData);
     }
   }
 
