cc5464c2772036b1d5e7781a596cd8dc1630afa8
denispo <denispolasek@centrum.cz>
2022-09-14 00:18:16 +0200

    wip

--- src/TigerApi/Payload/BaseTokenPayload.php
+++ src/TigerApi/Payload/BaseTokenPayload.php
@@ -7,7 +7,7 @@ use TigerCore\ValueObject\VO_TokenPlainStr;
 abstract class BaseTokenPayload extends TigerPayload {
 
   public function __construct(VO_TokenPlainStr $tokenStr) {
-    parent::__construct(['tkn' => $tokenStr]);
+    parent::__construct(['tkn' => $tokenStr->getValue()]);
   }
 
 }
