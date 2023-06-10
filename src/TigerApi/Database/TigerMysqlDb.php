4f0229047f54c35e49fc098c0b65b6ffa8565014
Denis <denispolasek@gmail.com>
2023-06-10 15:25:55 +0200

    MySql MYSQL_ATTR_FOUND_ROWS set to true

--- src/TigerApi/Database/TigerMysqlDb.php
+++ src/TigerApi/Database/TigerMysqlDb.php
@@ -10,7 +10,10 @@ class TigerMysqlDb implements ICanGetDbConnection {
   private Connection $db;
 
   public function __construct(IAmMySqlConnectionCredentials $connectionCredentials) {
-    $this->db = new Connection('mysql:host='.$connectionCredentials->getHost().';dbname='.$connectionCredentials->getDbName(), $connectionCredentials->getUserName(), $connectionCredentials->getPassword(),['lazy' => true]);
+    // \PDO::MYSQL_ATTR_FOUND_ROWS => TRUE means: UPDATE command will return affectedRows = 1 even if new and current value has the same value ie. nothing is actually updated.
+    // Default behavior is UPDATE return 0 AffectedRows if new value has the same value as current value stored in DB.
+    // This behavior cause unability to deretmine if record in DV was not found, or was found but contains already contains new value
+    $this->db = new Connection('mysql:host='.$connectionCredentials->getHost().';dbname='.$connectionCredentials->getDbName(), $connectionCredentials->getUserName(), $connectionCredentials->getPassword(),['lazy' => true, \PDO::MYSQL_ATTR_FOUND_ROWS => true]);
   }
 
   public function GetDbConnection(): Connection {
