a196238591a6c14db65ff4668cb0b823bbba077e
Denis <denispolasek@gmail.com>
2023-07-01 21:59:38 +0200

    Suppressed errors by @ are no longer reported by custom error handler

--- src/TigerApi/Error/TigerErrorHandler_TextHttpOutput.php
+++ src/TigerApi/Error/TigerErrorHandler_TextHttpOutput.php
@@ -8,9 +8,10 @@ use Nette\Http\IResponse;
 class TigerErrorHandler_TextHttpOutput implements ICanHandlePhpError {
 
   public function handlePhpError(int $errNo, string $errMsg, string $file, int $line):void{
-    if (error_reporting() === 0) {
-      // error was suppressed by @ so we do not want to report it
-      // https://stackoverflow.com/a/7422024
+    if (error_reporting() === 0 || error_reporting() === (E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR | E_PARSE)) {
+      // Error was suppressed by @ so we do not want to report it.
+      // ( Nette is still using @ to suppress errors. Unfortunately :/ )
+      // https://stackoverflow.com/a/74324388
       return;
     }
     $httpResponse = new \Nette\Http\Response();
