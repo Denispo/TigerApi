fbb82c0c26389ad77f74d7eef92d7bb6e1615cf7
Denis <denispolasek@gmail.com>
2023-10-09 00:39:36 +0200

    tokens wip

--- src/TigerApi/Auth/ATigerTokenJwtFirebaseCustomToken.php
+++ src/TigerApi/Auth/ATigerTokenJwtFirebaseCustomToken.php
@@ -2,48 +2,51 @@
 
 namespace TigerApi\Auth;
 
-use TigerCore\Auth\BaseJwtToken;
 use TigerCore\Auth\BaseTokenClaims;
 use TigerCore\Auth\FirebaseCustomToken;
-use TigerCore\Auth\ICanAddCustomTokenClaim;
+use TigerCore\Auth\ICanAddTokenClaim;
+use TigerCore\Auth\ICanGetTokenClaims;
 use TigerCore\Exceptions\InvalidArgumentException;
+use TigerCore\Exceptions\InvalidFileNameException;
+use TigerCore\Exceptions\InvalidFormatException;
 use TigerCore\Exceptions\InvalidTokenException;
-use TigerCore\ValueObject\VO_Duration;
 use TigerCore\ValueObject\VO_FullPathFileName;
 use TigerCore\ValueObject\VO_TokenPlainStr;
-use TigerCore\ValueObject\VO_TokenPrivateKey;
-use TigerCore\ValueObject\VO_TokenPublicKey;
 
-abstract class ATigerTokenJwtFirebaseCustomToken implements ICanGenerateFirebaseCustomToken , ICanDecodeFirebaseCustomToken {
+abstract class ATigerTokenJwtFirebaseCustomToken implements ICanGenerateFirebaseCustomToken , ICanGetTokenClaims {
 
-  protected abstract function onGetPublicKey():VO_TokenPublicKey;
+  /**
+   * @return VO_FullPathFileName|array{client_email:string,private_key:string}
+   */
   protected abstract function onGetFirebaseServiceAccountJson():VO_FullPathFileName|array;
-  protected abstract function onAddTokenCustomClaims(ICanAddCustomTokenClaim $claimCollector):void;
-
+  protected abstract function onAddTokenCustomClaims(ICanAddTokenClaim $claimCollector):void;
 
   /**
    * @param string|int $userId
    * @return VO_TokenPlainStr
    * @throws InvalidArgumentException
+   * @throws InvalidFileNameException
    * @throws InvalidTokenException
+   * @throws InvalidFormatException
    */
   public function generateToken(string|int $userId): VO_TokenPlainStr {
+    if (empty($userId)) {
+      throw new InvalidArgumentException('UserId can not be empty');
+    }
     $claims = new BaseTokenClaims();
     $this->onAddTokenCustomClaims($claims);
-    return FirebaseCustomToken::generateToken($this->onGetFirebaseServiceAccountJson(), $userId, $claims);
-  }
-
-  /**
-   * @param VO_TokenPlainStr $refreshToken
-   * @return BaseTokenClaims
-   */
-  public function decodeToken(VO_TokenPlainStr $refreshToken): BaseTokenClaims {
-    try {
-      $decodedToken =  FirebaseCustomToken::decodeToken($this->onGetPublicKey(),$refreshToken);
-    } catch (\Exception) {
-      return new BaseTokenClaims();
+    $serviceAccountData = $this->onGetFirebaseServiceAccountJson();
+    if ($serviceAccountData instanceof VO_FullPathFileName) {
+      $serviceAccountData = @file_get_contents($serviceAccountData->getValueAsString());
+      if ($serviceAccountData === false) {
+        throw new InvalidFileNameException('Can not readservice account file');
+      }
+      $serviceAccountData = json_decode($serviceAccountData,true);
+      if ($serviceAccountData === null) {
+        throw new InvalidFormatException('Invalid JSON format of service account file');
+      }
     }
-
+    return FirebaseCustomToken::generateToken($serviceAccountData, $userId, $claims);
   }
 
 }
--- src/TigerApi/Auth/ICanDecodeFirebaseIdToken.php
+++ src/TigerApi/Auth/ICanDecodeFirebaseIdToken.php
@@ -5,13 +5,13 @@ namespace TigerApi\Auth;
 use TigerCore\Exceptions\InvalidTokenException;
 use TigerCore\ValueObject\VO_TokenPlainStr;
 
-interface ICanDecodeAuthToken {
+interface ICanDecodeFirebaseIdToken {
 
   /**
-   * @param VO_TokenPlainStr $authToken
+   * @param VO_TokenPlainStr $token
    * @return TigerAuthTokenClaims
    * @throws InvalidTokenException
    */
-  public function decodeAuthToken(VO_TokenPlainStr $authToken):TigerAuthTokenClaims;
+  public function decodeFirebaseIdToken(VO_TokenPlainStr $token):TigerAuthTokenClaims;
 
 }
