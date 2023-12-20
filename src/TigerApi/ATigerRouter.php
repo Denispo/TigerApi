c8bc8018bbd4699ed970162570dec30dac3347b8
Denis <denispolasek@gmail.com>
2023-12-20 02:16:30 +0100

    ATigerRouter support only POST method

--- src/TigerApi/ATigerRouter.php
+++ src/TigerApi/ATigerRouter.php
@@ -12,27 +12,24 @@ abstract class ATigerRouter implements IAmTigerRouter {
 
   private BaseRestRouter $router;
 
-  abstract protected function onBeforeRunMatch(VO_HttpRequestMethod $requestMethod, string $requestUrlPath);
-
-  public function __construct()
+  public function __construct(private VO_RouteMask $pathPrefix)
   {
     $this->router = new BaseRestRouter();
   }
 
   /**
-   * @param 'PUT'|'GET'|'POST'|'DELETE'|'PATCH'|('PUT'|'GET'|'POST'|'DELETE'|'PATCH')[] $method
    * @param VO_RouteMask $mask
    * @param ICanHandleMatchedRoute $handler
    * @return void
    */
-  public function addRoute(string|array $method, VO_RouteMask $mask, ICanHandleMatchedRoute $handler)
+  public function addRoute(VO_RouteMask $mask, ICanHandleMatchedRoute $handler)
   {
-    $this->router->addRoute($method, $mask, $handler);
+    // Everzthing is POST. See Allan Holub presentation form 2015
+    $this->router->addRoute('POST', $this->pathPrefix->add($mask), $handler);
   }
 
   public function runMatch(VO_HttpRequestMethod $requestMethod, string $requestUrlPath): null|ICanGetPayloadRawData
   {
-    $this->onBeforeRunMatch($requestMethod, $requestUrlPath);
     return $this->router->runMatch($requestMethod, $requestUrlPath);
   }
 
