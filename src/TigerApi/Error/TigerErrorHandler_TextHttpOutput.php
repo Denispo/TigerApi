e4934985945edd1aaf1b0053b891fd95818fff1b
Denis <denispolasek@gmail.com>
2024-01-31 23:10:37 +0100

    Error handler throws exception 500

--- src/TigerApi/Error/TigerErrorHandler_TextHttpOutput.php
+++ src/TigerApi/Error/TigerErrorHandler_TextHttpOutput.php
@@ -3,10 +3,13 @@
 namespace TigerApi\Error;
 
 
-use Nette\Http\IResponse;
+use TigerCore\Response\S500_InternalServerErrorException;
 
 class TigerErrorHandler_TextHttpOutput implements ICanHandlePhpError {
 
+  /**
+   * @throws S500_InternalServerErrorException
+   */
   public function handlePhpError(int $errNo, string $errMsg, string $file, int $line):void{
     if (error_reporting() === 0 || error_reporting() === (E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR | E_PARSE)) {
       // Error was suppressed by @ so we do not want to report it.
@@ -14,12 +17,7 @@ class TigerErrorHandler_TextHttpOutput implements ICanHandlePhpError {
       // https://stackoverflow.com/a/74324388
       return;
     }
-    $httpResponse = new \Nette\Http\Response();
-    $httpResponse->setHeader('Access-Control-Allow-Origin','*');
-    $httpResponse->setHeader('Access-Control-Allow-Headers','*');
-    $httpResponse->setContentType('text/plain','utf-8');
-    $httpResponse->setCode(IResponse::S500_InternalServerError);
-    echo "Wow my custom error handler got #[$errNo] occurred in [$file] at line [$line]: [$errMsg]";
+    throw new S500_InternalServerErrorException('Unhandled app error occured',['$errNo'=>$errNo,'$file'=>$file,'$line'=>$line,'$errMsg'=>$errMsg]);
   }
 
 }
