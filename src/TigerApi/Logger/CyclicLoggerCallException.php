20f122916b7efa242afb01fa6324347eb45c5810
Denis <denispolasek@gmail.com>
2024-06-23 16:20:38 +0200

    BaseTigerException

--- src/TigerApi/Logger/CyclicLoggerCallException.php
+++ src/TigerApi/Logger/CyclicLoggerCallException.php
@@ -2,9 +2,9 @@
 
 namespace TigerApi\Logger;
 
-use TigerCore\Exceptions\_BaseException;
+use TigerCore\Exceptions\BaseTigerException;
 
-class CyclicLoggerCallException extends _BaseException {
+class CyclicLoggerCallException extends BaseTigerException {
 
   public function __construct(string $message, public BaseLogData $calledFrom) {
     parent::__construct($message);
