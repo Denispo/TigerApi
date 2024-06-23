ecf6adddd41fdc06c9d8546bd899a60e81e320f2
Denis <denispolasek@gmail.com>
2024-06-23 17:30:38 +0200

    LogDataException.php

--- src/TigerApi/Logger/LogDataException.php
+++ src/TigerApi/Logger/LogDataException.php
@@ -7,7 +7,7 @@ class LogDataException extends BaseLogData {
   public function __construct(\Throwable $exception, array $customData = []) {
     parent::__construct(
        message: $exception->getMessage(),
-       customData: ['CUSTOMDATA' => $customData, 'EXCEPTION_TRACE' => $exception->getTrace()],
+       customData: $customData,
        exception: $exception,
        file: $exception->getFile(),
        line: $exception->getLine()
